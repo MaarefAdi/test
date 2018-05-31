d2f1c07554a73def666f2bcba711cdc79a11c92a
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-31 22:52:00 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -20,26 +20,8 @@ messaging.requestPermission().then(function() {
   return messaging.getToken();
   .then (function(token){
     console.log(token);
-  })
+  });
 }).catch(function(err) {
   console.log('Unable to get permission to notify.', err);
 
-});
-// Get Instance ID token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-messaging.getToken().then(function(currentToken) {
-  if (currentToken) {
-    sendTokenToServer(currentToken);
-    updateUIForPushEnabled(currentToken);
-  } else {
-    // Show permission request.
-    console.log('No Instance ID token available. Request permission to generate one.');
-    // Show permission UI.
-    updateUIForPushPermissionRequired();
-    setTokenSentToServer(false);
-  }
-}).catch(function(err) {
-  console.log('An error occurred while retrieving token. ', err);
-  showToken('Error retrieving Instance ID token. ', err);
-  setTokenSentToServer(false);
 });
