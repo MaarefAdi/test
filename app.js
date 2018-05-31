a7493622756ea1619fbc771a8a7ea13d4d82387c
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-31 11:14:02 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+
 // Rigister the Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
