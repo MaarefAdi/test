b7f732c0416e6d5b19d7c10dc38cb1d93d0d385a
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-31 10:39:27 +0100

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,33 +1,12 @@
-// Files to cache
-var cacheName = 'Adi-app-v1';
-var appShellFiles = [
-  '/test/index.html'
-  
-];
+/// Register a Service Worker.
+navigator.serviceWorker.register('service-worker.js');
 
-// Installing Service Worker
-self.addEventListener('install', function(e) {
-  console.log('[Service Worker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(appShellFiles);
-    })
-  );
-});
-
-// Fetching content using Service Worker
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+navigator.serviceWorker.ready
+.then(function(registration) {
+  // Use the PushManager to get the user's subscription to the push service.
+  return registration.pushManager.getSubscription()
+  .then(async function(subscription) {
+    // If a subscription was found, return it.
+    if (subscription) {
+      return subscription;
+    };
