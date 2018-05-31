d3bd8673ece54cabbd09ad41c1f237e594b5c3a8
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-31 22:48:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBf93BVzxnWkD0XbYbdapel5s_iq8reRm8",
+  authDomain: "adi-app-b3698.firebaseapp.com",
+  databaseURL: "https://adi-app-b3698.firebaseio.com",
+  projectId: "adi-app-b3698",
+  storageBucket: "adi-app-b3698.appspot.com",
+  messagingSenderId: "642187156042"
+};
+firebase.initializeApp(config);
+
+
 // Retrieve Firebase Messaging object.
 const messaging = firebase.messaging();
 // Add the public key generated from the console here.
@@ -5,7 +17,10 @@ messaging.usePublicVapidKey("BIS-utsXNZDY5rcU3OeKUKki5cycQN9NJ17ENhNeuh096qqLDNz
 messaging.requestPermission().then(function() {
   console.log('Notification permission granted.');
   // TODO(developer): Retrieve an Instance ID token for use with FCM.
-  // ...
+  return messaging.getToken();
+  .then (function(token){
+    console.log(token);
+  })
 }).catch(function(err) {
   console.log('Unable to get permission to notify.', err);
 
